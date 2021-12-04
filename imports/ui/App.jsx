1dd14e22fe02d27527401740ea3349c3dc3147f9
azhar-azad <azhar.u.azad@gmail.com>
2021-12-04 17:23:10 +0600

    render tasks collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '../api/TasksCollection';
 import { Task } from './Task.jsx';
 
-// As we are not connecting to our server and our
-// database yet let's define some sample data which will
-// be used to render a list of tasks. 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'}
-];
+export const App = () => {
+  // The useTracker function exported by 
+  // react-meteor-data is a React Hook that allows us
+  // to have reactivity in our React components. Every
+  // time the data changes through reactivity our
+  // component will re-render. 
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
 
-    <ul>
-      { tasks.map(task => <Task key={task._id} task={task}/>) }
-    </ul>
-  </div>
-);
+      <ul>
+        { tasks.map(task => <Task key={task._id} task={task}/>) }
+      </ul>
+    </div>
+  );
+};
