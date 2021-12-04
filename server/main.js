466dc52b9d6deca5bbf7ae03329406e99dc0d1c9
azhar-azad <azhar.u.azad@gmail.com>
2021-12-05 01:18:27 +0600

    create user account

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
+
+const SEED_USERNAME = 'meteor';
+const SEED_PASSWORD = 'meteor';
 
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
