d4913ba7edfed3a520b9a000f09d5f3115c70ddd
azhar-azad <azhar.u.azad@gmail.com>
2021-12-05 02:19:58 +0600

    add task methods

---
+++ imports/api/tasksMethods.js
@@ -0,0 +1,41 @@
+import { check } from 'meteor/check';
+import { TasksCollection } from './TasksCollection';
+
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized');
+    }
+
+    TasksCollection.insert({
+      text,
+      createdAt: new Date,
+      userId: this.userId
+    });
+  },
+
+  'tasks.remove'(taskId) {
+    check(text, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized');
+    }
+
+    TasksCollection.remove(taskId);
+  },
+
+  'tasks.setIsChecked'(taskId, isChecked) {
+    check(text, String);
+    check(isChecked, Boolean);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized');
+    }
+
+    TasksCollection.update(taskId, {
+      $set: { isChecked }
+    });
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import '../imports/api/tasksMethods'; // registering the methods
 
 const SEED_USERNAME = 'meteor';
 const SEED_PASSWORD = 'meteor';
