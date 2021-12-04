1406afdddbe587bb81144fe41017a96c85ed3806
azhar-azad <azhar.u.azad@gmail.com>
2021-12-05 00:14:43 +0600

    add checkbox

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -5,8 +5,17 @@
 
 import React from "react";
 
-export const Task = ({ task }) => {
+export const Task = ({ task, onCheckboxClick }) => {
   // As this component will be inside a list
   // we are returning a li element. 
-  return <li>{task.text}</li>
+  return (
+    <li>
+      <input 
+        type="checkbox"
+        checked={!!task.isChecked}
+        onClick={() => onCheckboxClick(task)}
+        readOnly />
+      <span>{task.text}</span>
+    </li>
+  )
 };
