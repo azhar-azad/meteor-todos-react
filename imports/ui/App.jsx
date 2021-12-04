d6f9f91ff6c465009d8141831f8192c1f972d1d6
azhar-azad <azhar.u.azad@gmail.com>
2021-12-04 16:52:37 +0600

    create sample tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
+// As we are not connecting to our server and our
+// database yet let's define some sample data which will
+// be used to render a list of tasks. 
+const task = [
+  {_id: 1, text: 'First Task'},
+  {_id: 2, text: 'Second Task'},
+  {_id: 3, text: 'Third Task'}
+];
+
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
